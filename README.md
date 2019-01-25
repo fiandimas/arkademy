dc9ea15dd960979a85c93d6ea42d4d7c31880a9a
Alfian Dimas <kidaliez@gmail.com>
2019-01-26 01:17:00 +0700

    update soal 5

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# arkademy" 
+Arkademy
--- soal5.php
+++ soal5.php
@@ -2,9 +2,15 @@
 
   function countSameLetters($letters,$find){
     $result = '';
-    for($i=0;$i<strlen($letters);$i++){
-      if($letters[$i] == $find){
-        $result++;
+    $counter = 1;
+    for($i=0;$i<$counter;$i++){
+      if(@$letters[$i] != ""){
+        if(@$letters[$i] == $find){
+          $result++;
+        }
+        $counter++;
+      }else{
+        break;
       }
     }
     return "found ($result) of ($find) in ($letters)";
