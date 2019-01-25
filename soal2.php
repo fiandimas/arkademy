b16413b0daa923ae2cb3123a38cf1627bafd1766
Alfian Dimas <kidaliez@gmail.com>
2019-01-26 02:56:28 +0700

    update soal 2

--- soal2.php
+++ soal2.php
@@ -1,21 +1,28 @@
 <?php
 
+
   function verifUsername($username){
-    $fivelower = '';
-    for($i=0;$i<5;$i++){
-      $fivelower .= strtolower($username[$i]);
-    }
-    $rand = rand(0,1);
-    $char = array(
-      '0' => '.',
-      '1' => '_'
-    );
-    $fivelower .= $char[$rand];
-    $twoupper = range('A','Z');
-    $result =  $fivelower.$twoupper[rand(0,25)].$twoupper[rand(0,25)];
-    return $result;
+    $five = substr($username,0,5);
+    $char = substr($username,5,1);
+    $two = substr($username,6,1).substr($username,7,1);
+    if(strlen($username) > 8){
+      return 'Username harus terdiri dari 8 Karakter. 5 huruf awalan huruf kecil, dilanjutkan dengan titik (.) atau garis bawah (_), diteruskan dengan 2 huruf besar. contoh dimas.AD';
+    }else if(preg_match('/[A-Z]/', $five)){
+     return '5 Karakter pertama harus huruf kecil';
+    }else if(preg_match('/[a-z]/',$two)){
+      return '2 Huruf dari belakang harus huruf besar';
+    }else if($char != "." && $char != "_"){
+      return '3 Karakter dari belakang setidaknya harus mengandung titik (.) atau garis bawah (_)';
+    }else{
+      return 'Username benar';
+    } 
   }
-
-  echo verifUsername('DIMAS');
-
-  
+  echo verifUsername('ddimas.AD');
+  echo "\n";
+  echo verifUsername('Dimas.AD');
+  echo "\n";
+  echo verifUsername('dimas.ad');
+  echo "\n";
+  echo verifUsername('dimas-AD');
+  echo "\n";
+  echo verifUsername('dimas.AD');
